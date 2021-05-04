53b7ba918b54a6c97b30dec7bbe1649f2893c8f0
adamkacik <45773603+adamkacik@users.noreply.github.com>
2021-05-04 23:27:09 +0200

    Add mouse table
    Add QWidgetTable to GUI and pandas DataFrame table

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 from PyQt5.QtGui import QPalette, QColor, QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QStyle, QTableWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QStyle, QTableWidget, \
+    QTableWidgetItem
 from PyQt5.QtCore import Qt
 from pynput import mouse, keyboard
-
+import pandas
+import time
 
 class Window(QWidget):
 
@@ -11,6 +13,13 @@ class Window(QWidget):
         super().__init__()
         self.setWindowTitle("Mouse and Keyboard Recorder")
         self.setWindowIcon(self.style().standardIcon(QStyle.SP_DesktopIcon))
+        self.events_table = pandas.DataFrame(columns=['Kind', 'Type', 'Button', 'Coordinates', 'Wait', 'Description', 'Comment'])
+
+        # self.events_table = {'Kind': '',
+        #                      'Type': '',
+        #                      'Coordinates': (),
+        #                      'Scroll': (),
+        #                      'Wait': ''}
 
         self.mouse_listener = mouse.Listener(
             on_move=self.on_move,
@@ -54,15 +63,14 @@ class Window(QWidget):
         setting_button.clicked.connect(self.settings_record)
         btn_box.addWidget(setting_button)
 
-        header_list = ['Kind', 'Button', 'Values', 'Wait', 'Description', 'Comment']
         self.step_table = QTableWidget()
-        self.step_table.setColumnCount(len(header_list))
-        self.step_table.setHorizontalHeaderLabels(header_list)
-        for i in range(len(header_list)):
+        self.step_table.setColumnCount(len(self.events_table.columns))
+        self.step_table.setHorizontalHeaderLabels(self.events_table.columns)
+        for i in range(len(self.events_table.columns)):
             self.step_table.setColumnWidth(i, column_width := 250)
 
         # only for visible all tables column in GUI
-        width = (column_width * len(header_list) + 34)
+        width = (column_width * len(self.events_table.columns) + 100)
         height = 700
         self.setGeometry(400, 800, width, height)
 
@@ -75,15 +83,68 @@ class Window(QWidget):
     def start_record(self):
         print(f'→ {self.start_record.__name__}')
         self.mouse_listener.start()
+        self.on_time = time.time()
 
-    def on_move(self, *args):
-        print('move', *args)
 
-    def on_click(self, *args):
-        print('click', *args)
 
-    def on_scroll(self, *args):
-        print('scroll', *args)
+
+    def on_move(self, x, y):
+        # self.on_time = time.time() - self.on_time
+        # self.events_table = self.events_table.append({'Kind': 'Mouse',
+        #                           'Type': 'Move',
+        #                           'Coordinates': (x, y),
+        #                           'Wait': f'{self.on_time:.3f}',
+        #             'Description': '',
+        #             'Comment': ''},
+        #                           ignore_index=True)
+        self.on_time = time.time()
+
+    def on_click(self, x, y, button, pressed):
+        self.on_time = time.time() - self.on_time
+        print(f'{self.on_time:.3f}', pressed)
+        self.events_table = self.events_table.append({
+            'Kind': 'Mouse',
+            'Button': button,
+            'Type': pressed,
+            'Coordinates': (x, y),
+            'Wait': f'{self.on_time:.3f}',
+            'Description': '',
+            'Comment': ''},
+            ignore_index=True)
+        self.on_time = time.time()
+
+    def on_scroll(self, x, y, dx, dy):
+        self.on_time = time.time() - self.on_time
+        self.events_table = self.events_table.append({
+            'Kind': 'Mouse',
+            'Coordinates': (x, y),
+            'Button': (dx, dy),
+            'Type': 'Scroll',
+            'Wait': f'{self.on_time:.3f}',
+            'Description': '',
+            'Comment': ''},
+            ignore_index=True)
+        self.on_time = time.time()
+    # {'Kind': 'Mouse',
+    # 'Type': 'Move, RMB(Press/Release), LMB(Press/Release), Middle(Press/Release),Scroll',
+    # 'Coordinates' : (x, y),
+    # 'Scroll' : (0,1),
+    # 'Wait': 'time'}
+# move x, y
+# click x y button.left/right true/false
+# scroll x y 0 -1
+# time
+# 0:52  1984 - George Orwell
+# 1:55  Grona gniewu - John Steinbeck
+# 2:36 Miłość W Czasach Zarazy - Garcia Marquez
+# 3:21 Paragraf 22 - Joseph Heller
+# 3:55 Zaproszenie na egzekucję - Vladimir Nabokov
+# 5:07 Bracia Karamazow - Fiodor Dostojewski (nie kupujcie wydania z ilustracji na filmie)
+# 6:09 Śmieszne miłości - Milan Kundera (żarcik może nawet absurdalny: "i jeszcze jego inspiracje czerpane z Nietzschego" - jak to z niczego ? przecież z czegoś musiał czerpać)
+# 7:17 Atlas zbuntowany - Ayn Rand
+# 8:45 Syreny z Tytana - Kurt Vonnegut
+# 9:30 Obcy - Albert Camus
+
 
     def stop_record(self):
         print(f'→ {self.stop_record.__name__}')
@@ -92,7 +153,14 @@ class Window(QWidget):
             on_move=self.on_move,
             on_click=self.on_click,
             on_scroll=self.on_scroll)
+        # print(self.events_table)
+        self.step_table.setRowCount(len(self.events_table))
+        for number_row, row in self.events_table.iterrows():
+            for number_column, column in enumerate(row):
+                item = QTableWidgetItem(str(column))
+                self.step_table.setItem(number_row, number_column, item)
     # /LISTENER
+
 
     def play_record(self):
         print(f'→ {self.play_record.__name__}')
